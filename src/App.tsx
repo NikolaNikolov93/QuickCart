439c6f5ed9106525ce0aa37d65785ad9a4237453
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-05-22 01:28:22 +0300

    Fixed paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 
 import Navigation from "./features/navigaton/Navigation";
 import Home from "./pages/home/Home";
+import New from "./pages/new/New";
+import Categories from "./pages/categories/Categories";
 function App() {
   return (
     <div className="site">
@@ -10,6 +12,8 @@ function App() {
       <div className="main">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/categories" element={<Categories />} />
         </Routes>
       </div>
     </div>
