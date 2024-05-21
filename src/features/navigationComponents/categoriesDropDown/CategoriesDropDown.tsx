ca877e3e5080292d9b476551cfba3ab470cf6428
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-05-22 01:29:16 +0300

    Added categoreis dropdown menu

---
+++ src/features/navigationComponents/categoriesDropDown/CategoriesDropDown.tsx
@@ -0,0 +1,13 @@
+import styles from "./CategoriesDropDown.module.css";
+const CategoriesDropDown = () => {
+  return (
+    <div className={styles["dropdown"]}>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+    </div>
+  );
+};
+
+export default CategoriesDropDown;
--- src/features/navigationComponents/navigationLinks/NavigationLinks.tsx
+++ src/features/navigationComponents/navigationLinks/NavigationLinks.tsx
@@ -1,16 +1,37 @@
+import { NavLink } from "react-router-dom";
 import styles from "./NavigationLinks.module.css";
+import CategoriesDropDown from "../categoriesDropDown/CategoriesDropDown";
+import { useState } from "react";
 
 const NavigationLinks = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <ul className={styles["nav-links"]}>
-      <li>
-        <a href="">Home</a>
+      <li
+        onMouseEnter={() => setIsActive(true)}
+        onMouseLeave={() => setIsActive(false)}
+      >
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending ? styles["pending"] : isActive ? styles["active"] : ""
+          }
+          to="/categories"
+        >
+          Categories
+        </NavLink>
+        {isActive && <CategoriesDropDown />}
       </li>
+
       <li>
-        <a href="">Shop</a>
-      </li>
-      <li>
-        <a href="">About</a>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending ? styles["pending"] : isActive ? styles["active"] : ""
+          }
+          to="/new"
+        >
+          What's New
+        </NavLink>
       </li>
     </ul>
   );
