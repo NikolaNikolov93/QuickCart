0c51d3bab88749eeb9017f213c3eb3d4e5f0a38f
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-05-27 00:08:55 +0300

    Initialize home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,7 @@
+import styles from "./Home.module.css";
+
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-    </div>
-  );
+  return <section className={styles["home-page"]}></section>;
 };
 
 export default Home;
