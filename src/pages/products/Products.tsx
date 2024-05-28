30e6742588416da06392fa381e75cb21bb12e5b7
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-05-29 00:54:30 +0300

    Added sorting functionality

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -3,9 +3,11 @@ import styles from "./Products.module.css";
 import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/productCard/ProductCard";
+import sortBy from "../../helpers/sortBy";
 
 const Products = () => {
   const [productsList, setProductsList] = useState([]);
+  const [sortedList, setSortedList] = useState([]);
   const [loading, setLoading] = useState(true);
   const { products } = useParams();
 
@@ -21,6 +23,7 @@ const Products = () => {
         const result = await response.json();
 
         setProductsList(result.products);
+        setSortedList(result.products);
       } catch (error) {
         console.log(error);
       } finally {
@@ -30,15 +33,33 @@ const Products = () => {
     fetchData();
   }, [products]);
 
-  const productPage = useMemo(() => {
-    return productsList.map((product: any) => (
-      <ProductCard key={product.id} product={product} />
-    ));
-  }, [productsList]);
-
+  const handleSort = (criteria) => {
+    const sorted = sortBy(criteria, [...productsList]);
+    setSortedList(sorted);
+  };
   return (
     <>
-      <div className={styles["products-container"]}>{productPage}</div>
+      <div className={styles["sort-container"]}>
+        <label htmlFor="sort" className="sort-label">
+          Sort by:
+        </label>
+        <select
+          className={styles["sort"]}
+          name="sort"
+          onChange={(e) => {
+            handleSort(e.target.value);
+          }}
+        >
+          <option value="default">Choose sorting option..</option>
+          <option value="price-high-low">Price (High-Low)</option>
+          <option value="price-low-high">Price (Low-High)</option>
+        </select>
+      </div>
+      <div className={styles["products-container"]}>
+        {sortedList.map((product: any) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </>
   );
 };
