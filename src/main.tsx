ffe08f7d1eebafeeef3d01f57eedd83b762bdfba
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-06-17 07:22:30 +0300

    Added [Redux Persist]

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 import App from "./App.tsx";
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,12 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 import favouritesReducer from "./favourites/favouritesSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  user: userReducer,
+  favourtes: favouritesReducer,
+});
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    favourtes: favouritesReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
